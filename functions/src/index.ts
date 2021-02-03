2a97e340c1e3b362e7cd9b5a5debfabf48d9f83d
mfish33 <maxmfishernj@gmail.com>
2021-02-03 12:35:10 -0800

    updated firebaserc to set default project for all cases

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as functions from "firebase-functions";
 
 export const test = functions.https.onRequest(async (req, res) => {
-  res.sendStatus(200).send("Hello World");
+  res.send("Hello World");
 });
